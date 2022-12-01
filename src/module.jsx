182f9321783d97ddc0db2d3d3238a4454d2304b4
Anton.gap <anton.gap99@gmail.com>
2022-12-01 21:51:58 +0300

    feat: react webpack config

---
+++ src/module.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import img from './img/portfolio2.png';
+
+const Module = () =>(
+  <>
+    <h1>
+      hello, React!
+    </h1>
+    <img width={'300px'} src={img} alt="" />
+  </>
+);
+
+
+export default Module;
+
